9745f28119012d7b7caefc41936b48a824f8e5e3
Tobias Behr <tobias.behr@holisticon.de>
2019-12-03 13:01:56 +0100

    Day 2 and 3

--- src/advent_of_code_19/core.clj
+++ src/advent_of_code_19/core.clj
@@ -1,7 +1,13 @@
 (ns advent-of-code-19.core)
 
+
+(defn read-number-input [filename]
+  (map #(Integer. %) (clojure.string/split (slurp filename) #"\n")))
+
 (def input1 (map #(Integer. %) (clojure.string/split (slurp "resources/input1.txt") #"\n")))
 (def input1-test [1969]) ; result 654
+
+;;; Day 1
 
 (defn fuel-for-mass [mass]
   (int (- (Math/floor (/ mass 3)) 2)))
@@ -21,3 +27,97 @@
   (->> input
        (map fuel-for-module)
        (reduce +)))
+
+
+;;; Day 2
+(defn input2 []
+  (->> (clojure.string/split 
+        (first (clojure.string/split (slurp "resources/input2.txt") #"\n")) #",")
+       (map #(Integer. %))
+       (reduce conj []))
+  )
+
+(defn calculate-window [[opcode pos1 pos2 pos-res] input]
+  (let [opcodes {1 + 2 *}
+        val1 (nth input pos1)
+        val2 (nth input pos2)
+        op (get opcodes opcode)]
+    (assoc input pos-res (apply op [val1 val2]))))
+
+(defn calculator [input index]
+  (let [opcode (get input index)
+        pos1 (get input (+ index 1))
+        pos2 (get input (+ index 2))
+        pos-res (get input (+ index 3))]
+    (if (= 99 opcode)
+      (reduced input)
+      (calculate-window [opcode pos1 pos2 pos-res] input))))
+
+(defn calculate [input]
+  (reduce calculator input (range 0 (count input) 4)))
+
+(defn day2-1 []
+  (let [input (assoc (input2) 
+                     1 12
+                     2 2)]
+    (calculate input)))
+
+(defn day2-2 []
+  (for [noun (range 100)
+        verb (range 100)
+        :let [input (assoc (input2) 1 noun 2 verb)
+              result (calculate input)]
+        :when (= 19690720 (first result))]
+    (+ (* 100 noun) verb)))
+
+;;; Day 3
+(defn input3 []
+  (clojure.string/split (slurp "resources/input3.txt") #"\n"))
+
+(defn parse-direction [direction]
+  [(subs direction 0 1) (Integer. (subs direction 1))])
+
+(defn next-point [[x y :as point] [course distance]]
+  (cond
+    (= "R" course) [(+ x distance) y]
+    (= "L" course) [(- x distance) y]
+    (= "U" course) [x (+ y distance)]
+    (= "D" course) [x (- y distance)]))
+
+(defn next-points [start [course distance]]
+  (map #(next-point start [course %]) (map inc (range distance))))
+
+(defn route [directions]
+  (reduce (fn [route direction]
+            (into route (next-points (peek route) direction))) 
+          [[0 0]] 
+          (map parse-direction directions)))
+
+(defn route-index [route]
+  (reduce (fn [m [x y]] 
+            (update-in m [x] into [y])) 
+          {} route))
+
+(defn find-intersects [route route-map]
+  (reduce 
+   (fn [intersects [x y :as point]] 
+     (if (some #(= % y) (get route-map x))
+       (conj intersects point)
+       intersects)) 
+   [] route))
+
+(defn distance-to-port [[x1 y1]]
+  (+ (Math/abs (- x1 0)) (Math/abs (- y1 0))))
+
+; 149816 points for route 1
+(defn day3-1 []
+  (let [input (input3)
+        route1 (route (clojure.string/split (first input) #","))
+        route2 (route (clojure.string/split (last input) #","))]
+    (->> route1 
+        (route-index)
+        (find-intersects route2)
+        (map distance-to-port)
+        sort
+        (drop 1)
+        first)))
--- test/advent_of_code_19/core_test.clj
+++ test/advent_of_code_19/core_test.clj
@@ -14,3 +14,45 @@
     (is (= 2 (fuel-for-module 14)))
     (is (= 966 (fuel-for-module 1969)))
     (is (= 50346 (fuel-for-module 100756)))))
+
+
+(deftest calculate-window-test
+  (testing "calculate single window"
+    (is (= [2 0 0 0 99] (calculate-window [1 0 0 0] [1 0 0 0 99])))
+    (is (= [2 3 0 6 99] (calculate-window [2 3 0 3] [2 3 0 3 99])))
+    (is (= [2 4 4 5 99 9801] (calculate-window [2 4 4 5] [2 4 4 5 99 0])))))
+
+
+(deftest calculator-test
+  (testing "calculation"
+    (is (= [2 0 0 0 99] (calculator [1 0 0 0 99] 0)))))
+
+(deftest day2-1-test
+  (testing "calculation"
+    (is (= [30 1 1 4 2 5 6 0 99] (calculate [1 1 1 4 99 5 6 0 99])))
+    (is (= [30 1 1 4 2 5 6 0 99 1 2 2 3] (calculate [1 1 1 4 99 5 6 0 99 1 2 2 3])))))
+
+
+(deftest next-point-test
+  (testing "calculation of next point"
+    (is (= [1 0] (next-point [0 0] ["R" 1])))
+    (is (= [-1 0] (next-point [0 0] ["L" 1])))
+    (is (= [0 1] (next-point [0 0] ["U" 1])))
+    (is (= [0 -1] (next-point [0 0] ["D" 1])))))
+
+(deftest next-points-test
+  (testing "calculation of next points"
+    (is (= [[1 0] [2 0] [3 0]] (next-points [0 0] ["R" 3])))
+    (is (= [[1 0] [2 0]] (next-points [0 0] ["R" 2])))
+    (is (= [[-1 0] [-2 0] [-3 0]] (next-points [0 0] ["L" 3])))
+    (is (= [[0 -1] [0 -2] [0 -3]] (next-points [0 0] ["D" 3])))
+    (is (= [[0 1] [0 2] [0 3]] (next-points [0 0] ["U" 3])))))
+
+(deftest parse-direction-test
+  (testing "parsing directions"
+    (is (= ["R" 1] (parse-direction "R1")))
+    (is (= ["R" 12] (parse-direction "R12")))))
+
+(deftest next-locations-test
+  (testing ""
+    (is (= [[0 0] [1 0] [2 0]] (route ["R2"])))))
